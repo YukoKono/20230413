2d559907c692af9b615eba193e12596256411ba0
YukoKono <130989029+YukoKono@users.noreply.github.com>
2023-04-19 14:42:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](http://colab.research.google.com/github/<username>/blob/<branch>/<filepath to .ipynb>)
 
 import streamlit as st
 import openai
